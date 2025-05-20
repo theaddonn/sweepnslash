bcd8f5d26b326ef7452e9e3f687f9a953c0bf022
theaddon <theaddonn@gmail.com>
2025-05-20 05:44:56 +0200

    install regolith

---
+++ packs/data/gametests/src/weaponStats/tcc/copper_expansion.js
@@ -0,0 +1,32 @@
+// https://www.curseforge.com/minecraft-bedrock/addons/copper-tools
+
+export const copperTools = [
+    {
+        id: 'tcc_copper_tools:copper_sword',
+        attackSpeed: 1.6,
+        damage: 6,
+        isWeapon: true,
+        sweep: true,
+    },
+    {
+        id: 'tcc_copper_tools:copper_pickaxe',
+        attackSpeed: 1.2,
+        damage: 4,
+    },
+    {
+        id: 'tcc_copper_tools:copper_shovel',
+        attackSpeed: 1,
+        damage: 4.5,
+    },
+    {
+        id: 'tcc_copper_tools:copper_axe',
+        attackSpeed: 0.9,
+        damage: 9,
+        disableShield: true,
+    },
+    {
+        id: 'tcc_copper_tools:copper_hoe',
+        attackSpeed: 3,
+        damage: 1,
+    },
+];
---
+++ packs/data/gametests/src/weaponStats/tcc/vanilla_throwing_knives.js
@@ -0,0 +1,40 @@
+// https://www.curseforge.com/minecraft-bedrock/addons/vanilla-knives
+
+export const vanillaThrowingKnives = [
+    {
+        id: 'tcc_throwing_knives:wooden_throwing_knife',
+        attackSpeed: 1.8,
+        damage: 2,
+        isWeapon: true,
+    },
+    {
+        id: 'tcc_throwing_knives:golden_throwing_knife',
+        attackSpeed: 1.8,
+        damage: 2,
+        isWeapon: true,
+    },
+    {
+        id: 'tcc_throwing_knives:stone_throwing_knife',
+        attackSpeed: 1.8,
+        damage: 3,
+        isWeapon: true,
+    },
+    {
+        id: 'tcc_throwing_knives:iron_throwing_knife',
+        attackSpeed: 1.8,
+        damage: 4,
+        isWeapon: true,
+    },
+    {
+        id: 'tcc_throwing_knives:diamond_throwing_knife',
+        attackSpeed: 1.8,
+        damage: 5,
+        isWeapon: true,
+    },
+    {
+        id: 'tcc_throwing_knives:netherite_throwing_knife',
+        attackSpeed: 1.8,
+        damage: 6,
+        isWeapon: true,
+    },
+];
